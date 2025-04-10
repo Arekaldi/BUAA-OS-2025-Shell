90d8aba3c8d810aad40e74162db6fb11c0505e23
李一鸣 <23371041@buaa.edu.cn>
2025-04-10 14:35:10 +0800

    page2pa(pp) to pa

--- kern/pmap.c
+++ kern/pmap.c
@@ -219,7 +219,7 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 	/* Step 3: Assign the kernel virtual address of the page table entry to '*ppte'. */
 	/* Exercise 2.6: Your code here. (3/3) */
 
-	u_long pa = page2pa(pp) << 12;
+	u_long pa = page2pa(pp);
 
 	Pte *pgtbl = KADDR(pa);
 
