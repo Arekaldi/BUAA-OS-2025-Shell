fb9883a509930cc6c98cfafe9a82a4e889e8b2a2
李一鸣 <23371041@buaa.edu.cn>
2025-06-10 20:44:56 +0800

    feat: lab6

--- user/lib/fd.c
+++ user/lib/fd.c
@@ -144,10 +144,10 @@ int dup(int oldfdnum, int newfdnum) {
 	ova = fd2data(oldfd);
 	nva = fd2data(newfd);
 	/* Step 5: Dunplicate the data and 'fd' self from old to new. */
-	if ((r = syscall_mem_map(0, oldfd, 0, newfd, vpt[VPN(oldfd)] & (PTE_D | PTE_LIBRARY))) <
+	/* if ((r = syscall_mem_map(0, oldfd, 0, newfd, vpt[VPN(oldfd)] & (PTE_D | PTE_LIBRARY))) <
 	    0) {
 		goto err;
-	}
+	} */
 
 	if (vpd[PDX(ova)]) {
 		for (i = 0; i < PDMAP; i += PTMAP) {
@@ -163,6 +163,10 @@ int dup(int oldfdnum, int newfdnum) {
 		}
 	}
 
+	if ((r = syscall_mem_map(0, oldfd, 0, newfd, vpt[VPN(oldfd)] & (PTE_D | PTE_LIBRARY))) <
+	    0) {
+		goto err;
+	}
 	return newfdnum;
 
 err:
@@ -191,18 +195,24 @@ int read(int fdnum, void *buf, u_int n) {
 	struct Dev *dev;
 	struct Fd *fd;
 	/* Exercise 5.10: Your code here. (1/4) */
-	try(fd_lookup(fdnum, &fd));
-	try(dev_lookup(fd->fd_dev_id, &dev));
+	if ((r = fd_lookup(fdnum, &fd)) < 0) {
+		return r;
+	}
+
+	if ((r = dev_lookup(fd->fd_dev_id, &dev)) < 0) {
+		return r;
+	}
 
 	// Step 2: Check the open mode in 'fd'.
 	// Return -E_INVAL if the file is opened for writing only (O_WRONLY).
 	/* Exercise 5.10: Your code here. (2/4) */
-	if((fd->fd_omode & O_ACCMODE) == O_WRONLY)
+	if ((fd->fd_omode & O_ACCMODE) == O_WRONLY) {
 		return -E_INVAL;
+	}
 
 	// Step 3: Read from 'dev' into 'buf' at the seek position (offset in 'fd').
 	/* Exercise 5.10: Your code here. (3/4) */
-	r = (dev->dev_read(fd, buf, n, fd->fd_offset));
+	r = dev->dev_read(fd, buf, n, fd->fd_offset);
 
 	// Step 4: Update the offset in 'fd' if the read is successful.
 	/* Hint: DO NOT add a null terminator to the end of the buffer!
@@ -212,6 +222,7 @@ int read(int fdnum, void *buf, u_int n) {
 	if (r > 0) {
 		fd->fd_offset += r;
 	}
+
 	return r;
 }
 
